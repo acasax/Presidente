c5c29ab6ee3d0d459fe275a3ede99cb30f600ef3
aca sax <acasax@gmail.com>
2021-06-27 21:05:01 +0200

    db connection

--- src/main/java/Presidente/TransactionApi/App.java
+++ src/main/java/Presidente/TransactionApi/App.java
@@ -1,13 +1,11 @@
 package Presidente.TransactionApi;
+import Presidente.TransactionApi.DbFunctions;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        DbFunctions db = new DbFunctions(); 
+        db.connect();
     }
 }
