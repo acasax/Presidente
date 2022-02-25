0119822a332f22916393d5d42d1ada365de31d19
acasax <acasax@gmail.com>
2022-02-25 13:57:36 +0100

    fix work time bug

--- src/main/java/Presidente/TransactionApi/spStart.java
+++ src/main/java/Presidente/TransactionApi/spStart.java
@@ -6,19 +6,17 @@ import java.sql.SQLException;
 import java.text.ParseException;
 import java.util.ArrayList;
 
-
 public class spStart extends Thread {
-	
+
 	static Object pgconn;
 	static Connection lConn;
 	static String spWithStatus0;
 	static int reportIndex;
 	static ArrayList<spProcessing> lista = new ArrayList<>();
 
-	Functions fun  = new Functions();
+	Functions fun = new Functions();
 	DbFunctions db = new DbFunctions();
-	
-	
+
 	// Da li postoji proces sa zadatim report index koji radi
 	public static spProcessing nadjiProcessing(int reportIndex) {
 		for (int i = 0; i < lista.size(); i++) {
@@ -38,59 +36,52 @@ public class spStart extends Thread {
 
 	@Override
 	public void run() {
-		try {
-			if(fun.workTime()) {
-				//Startuje proveru za slot periodic
+		// Startuje proveru za slot periodic
+		//
+		spCheck sk = new spCheck();
+		sk.start();
+		while (true) {
+			try {
+
+				// Slanje zahteva na putanju slot-periodic
 				//
-				spCheck sk     = new spCheck();
-				sk.start();
-				while(true) {
-					try {
+				fun.sendSlotPeriodicWithStatus0(reportIndex, db, lista);
 
-						//Slanje zahteva na putanju slot-periodic
-						//
-						fun.sendSlotPeriodicWithStatus0(reportIndex, db, lista);
-
-					} catch (SecurityException | IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-						try {
-							fun.createLog(e.getMessage());
-						} catch (SecurityException | IOException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						}
-					} catch (SQLException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-						try {
-							fun.createLog(e.getMessage());
-						} catch (SecurityException | IOException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						}
-					}
-					
-					try {
-						//cekaj 5 minuta pa uradi ponovo
-						//
-						Thread.sleep(300000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						try {
-							fun.createLog(e.getMessage());
-						} catch (SecurityException | IOException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						}
-					}
+			} catch (SecurityException | IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				try {
+					fun.createLog(e.getMessage());
+				} catch (SecurityException | IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				try {
+					fun.createLog(e.getMessage());
+				} catch (SecurityException | IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
 				}
 			}
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
+			try {
+				// cekaj 5 minuta pa uradi ponovo
+				//
+				Thread.sleep(300000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				try {
+					fun.createLog(e.getMessage());
+				} catch (SecurityException | IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
 		}
-		
+
 	}
 
 }
